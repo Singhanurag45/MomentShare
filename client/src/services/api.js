d28b7c35258121d6698153a8cf45c3f4c2ed5df0
Anurag singh <anuragsinghrbl2002.in>
2025-10-12 00:01:35 +0530

    add_env_render_backend_url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Your backend API URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, 
 });
 
 // Interceptor to add the token to every request
